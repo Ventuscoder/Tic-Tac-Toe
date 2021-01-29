45e430818bbce8789798c51182f005cd74d52fa4
ventuscoder <shivankcode@gmail.com>
2021-01-29 21:00:05 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function game() {
+    // alert("Please note that standard tic-tac-toe rules apply");
+    // alert("You will be playing against AI and you have the first move. Let the game commence!");
+}
+window.addEventListener('load', game);
