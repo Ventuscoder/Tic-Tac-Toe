46a92114d291b26d039b08c1628628ecd311a153
ventuscoder <shivankcode@gmail.com>
2021-01-31 20:01:42 +0530

    Added array variable for box buttons

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const boxes = document.querySelectorAll('.box');
+
 function game() {
     // alert("Please note that standard tic-tac-toe rules apply");
     // alert("You will be playing against AI and you have the first move. Let the game commence!");
