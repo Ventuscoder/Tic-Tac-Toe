29acec566d7cfea690f6cdbbc29f85b545be7ad7
ventuscoder <shivankcode@gmail.com>
2021-01-31 20:23:01 +0530

    Added function for reading index number and logging to console

--- script.js
+++ script.js
@@ -6,9 +6,20 @@ function addClasses() {
     }
 }
 
+function parseIndex(classList) {
+    console.log(classList[1]);
+}
+
 function game() {
     addClasses();
     // alert("Please note that standard tic-tac-toe rules apply");
     // alert("You will be playing against AI and you have the first move. Let the game commence!");
 }
+
+boxes.forEach(box => {
+    box.addEventListener('click', function(e) {
+        parseIndex(e.target.classList);
+    });
+});
+
 window.addEventListener('load', game);
