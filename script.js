68c948a393a24c6eee64912eb7ed7af724e9ca8c
ventuscoder <shivankcode@gmail.com>
2021-01-31 20:10:28 +0530

    Added function for adding class corresponding to square number

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const boxes = document.querySelectorAll('.box');
 
+function addClasses() {
+    for (let i = 1; i <= 9; i++) {
+        boxes[i-1].classList.add(`${i}`);
+    }
+}
+
 function game() {
+    addClasses();
     // alert("Please note that standard tic-tac-toe rules apply");
     // alert("You will be playing against AI and you have the first move. Let the game commence!");
 }
